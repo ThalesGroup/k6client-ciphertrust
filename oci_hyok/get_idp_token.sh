251362b27667d60887be0a951b8c812932a7888f
Misha Gupta <misha.gupta@thalesgroup.com>
2024-05-13 12:34:17 +0530

    Adding the K6 scripts to capture the OCI HYOK performance metrics

--- oci_hyok/get_idp_token.sh
+++ oci_hyok/get_idp_token.sh
@@ -15,5 +15,5 @@ set -e
 GRANT_TYPE="client_credentials"
 SCOPE="CTMoci_hyok_encrypt"
 
-cmd=$(curl --request POST --url "https://idcs-7d5ad58bec1b43efae281bb6fec8f42b.identity.oraclecloud.com:443/oauth2/v1/token" --header "content-type: application/x-www-form-urlencoded" --data grant_type=$GRANT_TYPE --data client_id=$IDP_ACCESS_ID --data client_secret=$IDP_SECRET_KEY --data scope=$SCOPE)
+cmd=$(curl --request POST --url "https://idcs-<id>.identity.oraclecloud.com:443/oauth2/v1/token" --header "content-type: application/x-www-form-urlencoded" --data grant_type=$GRANT_TYPE --data client_id=$IDP_ACCESS_ID --data client_secret=$IDP_SECRET_KEY --data scope=$SCOPE)
 echo $cmd
